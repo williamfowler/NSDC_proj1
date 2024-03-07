33c0bbd87cd2b21f2319ba397fba929aa582e5d9
William Fowler <williamfowler04@gmail.com>
2024-03-07 18:57:27 -0500

    changed prob2 to use tfidft and random forrest

--- prob2_feature_extractor.py
+++ prob2_feature_extractor.py
@@ -1,32 +1,49 @@
-import pickle
 import numpy as np
-from gensim.models import Word2Vec
 
-# takes average of vector for all words in a sentence
-def average_word_vectors(words, model, vocabulary, num_features):
-    feature_vector = np.zeros((num_features,), dtype="float64")
-    nwords = 0.
-    
-    for word in words:
-        if word in vocabulary: 
-            nwords = nwords + 1.
-            feature_vector = np.add(feature_vector, model.wv[word])
-    
-    if nwords:
-        feature_vector = np.divide(feature_vector, nwords)
-        
-    return feature_vector
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
 
-def feature_extractor2(x_train_df):
-    with open('word2vec_model.pkl','rb') as f:
-        word_2_vec = pickle.load(f)
+
+def feature_extractor2(x_train_df): 
+    with open('vocab2.pkl','rb') as f:
+        vocabulary = pickle.load(f)
     # print(x_train_df)
     x_text = []
     for n in x_train_df:
         # n.pop(0)
         x_text.append(n[1])
+
+    vectorizer = CountVectorizer(vocabulary=vocabulary)
+
+    x = vectorizer.transform(x_text)  
+    return x
+# from gensim.models import Word2Vec
+
+# # takes average of vector for all words in a sentence
+# def average_word_vectors(words, model, vocabulary, num_features):
+#     feature_vector = np.zeros((num_features,), dtype="float64")
+#     nwords = 0.
     
-    feature_vectors = [average_word_vectors(review, word_2_vec, word_2_vec.wv.key_to_index, 100) for review in x_text]
-    return np.stack(feature_vectors)
+#     for word in words:
+#         if word in vocabulary: 
+#             nwords = nwords + 1.
+#             feature_vector = np.add(feature_vector, model.wv[word])
+    
+#     if nwords:
+#         feature_vector = np.divide(feature_vector, nwords)
+        
+#     return feature_vector
+
+# def feature_extractor2(x_train_df):
+#     with open('word2vec_model.pkl','rb') as f:
+#         word_2_vec = pickle.load(f)
+#     # print(x_train_df)
+#     x_text = []
+#     for n in x_train_df:
+#         # n.pop(0)
+#         x_text.append(n[1])
+    
+#     feature_vectors = [average_word_vectors(review, word_2_vec, word_2_vec.wv.key_to_index, 100) for review in x_text]
+#     return np.stack(feature_vectors)
 
 
